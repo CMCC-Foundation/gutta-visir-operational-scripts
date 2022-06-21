16cc46802c6e6c187c576fedfc575e94cc03821e
Megi Hoxhaj <megi.hoxhaj@cmcc.it>
2022-06-21 12:35:33 +0200

    Check_v4 script:ignore warning about imagick library present in /log_path/err/GUTTA_xxx.err and avoid to report them in telegram channel

--- check_v4.sh
+++ check_v4.sh
@@ -48,71 +48,76 @@ fi
 for COMP in ${COMPONENTS[@]}; do
 
     # log/err file names
+	#echo "find ${LOG_PATH} -name ${COMP}_${LASTRUN}_\* | head -n 1"
     LOG=$(find ${LOG_PATH} -name ${COMP}_${LASTRUN}_\* | head -n 1)
     ERR=$(find ${ERR_PATH} -name ${COMP}_${LASTRUN}_\* | head -n 1)
 
-    # get termination signals for the component
-    TermSig=$(grep "Terminated" $LOG | wc -l)
+	if [[ ! -z "$LOG"  ]] ; then 
+		# get termination signals for the component
+		TermSig=$(grep "Terminated" $LOG | wc -l)
+		case $COMP in
+		
+		"campi")
+			if [[ $TermSig -eq 1 ]]; then
+			echo "$COMP terminated"
+			else
+			echo "$COMP still to complete! Exiting..."
+			exit
+			fi	    
+			;;
+		"tracce")
+			if [[ $TermSig -eq 30 ]]; then
+			echo "$COMP terminated"
+			else
+			echo "$COMP still to complete! Exiting..."
+			exit
+			fi
+			;;
+		"visual")
+			if [[ $TermSig -eq 30 ]]; then
+			echo "$COMP terminated"
+			else
+			echo "$COMP still to complete! Exiting..."
+			exit
+			fi
+			;;
+		"csv2shape")
+			if [[ $TermSig -eq 1 ]]; then
+			echo "$COMP terminated"
+			else
+			echo "$COMP still to complete! Exiting..."
+			exit
+			fi
+			;;
+		"copyToN08")
+			if [[ $TermSig -eq 1 ]]; then
+			echo "$COMP terminated"
+			else
+			echo "$COMP still to complete! Exiting..."
+			exit
+			fi
+			;;
+		"GUTTA_n08")
+			if [[ $TermSig -eq 1 ]]; then
+			echo "$COMP terminated"
+			else
+			echo "$COMP still to complete! Exiting..."
+			exit
+			fi
+			;;
+		*)
+			echo "ELSE"
+			;;
+		esac
+	fi 
+
     
-    case $COMP in
-	
-	"campi")
-	    if [[ $TermSig -eq 1 ]]; then
-		echo "$COMP terminated"
-	    else
-		echo "$COMP still to complete! Exiting..."
-		exit
-	    fi	    
-	    ;;
-	"tracce")
-	    if [[ $TermSig -eq 30 ]]; then
-		echo "$COMP terminated"
-	    else
-		echo "$COMP still to complete! Exiting..."
-		exit
-	    fi
-	    ;;
-	"visual")
-	    if [[ $TermSig -eq 30 ]]; then
-		echo "$COMP terminated"
-	    else
-		echo "$COMP still to complete! Exiting..."
-		exit
-	    fi
-	    ;;
-	"csv2shape")
-	    if [[ $TermSig -eq 30 ]]; then
-		echo "$COMP terminated"
-	    else
-		echo "$COMP still to complete! Exiting..."
-		exit
-	    fi
-	    ;;
-	"copyToN08")
-	    if [[ $TermSig -eq 1 ]]; then
-		echo "$COMP terminated"
-	    else
-		echo "$COMP still to complete! Exiting..."
-		exit
-	    fi
-	    ;;
-	"GUTTA_n08")
-	    if [[ $TermSig -eq 1 ]]; then
-		echo "$COMP terminated"
-	    else
-		echo "$COMP still to complete! Exiting..."
-		exit
-	    fi
-	    ;;
-	*)
-	    echo "ELSE"
-	    ;;
-    esac
 done
 
 # check if all the components stopped running
 for COMP in ${COMPONENTS[@]}; do
 
+	echo "========"
     # log/err file names
     LOG=$(find ${LOG_PATH} -name ${COMP}_${LASTRUN}_\* | head -n 1)
     ERR=$(find ${ERR_PATH} -name ${COMP}_${LASTRUN}_\* | head -n 1)
@@ -120,7 +125,7 @@ for COMP in ${COMPONENTS[@]}; do
     echo "Parsing log files:"
     echo $LOG
     echo $ERR
-    echo "========"
+    
     
     # do the analysis
     echo " - Analysing component $COMP"    
@@ -128,10 +133,22 @@ for COMP in ${COMPONENTS[@]}; do
     echo " - $COMP has  $ERRORS errors."
     if [[ ! -z $ERRORS ]]; then
         if [[ $ERRORS -gt 0 ]]; then
-            echo " -- Found $ERRORS errors: MUST BE NOTIFIED!"
-            echo $LASTRUN > ${LOG_PATH}/last_job_notified.log
-            Notify 2 "Check" "${COMP} has ${ERRORS} error lines [run: $LASTRUN]"   
-	fi
+            
+			WARNING_TO_IGNORE=$(grep "imagick.so" $ERR | wc -l)
+			ERRORS_UPDATE=$(echo "$ERRORS - $WARNING_TO_IGNORE" | bc -l )
+			#echo " -- $ERRORS_UPDATE errors about imagick.so library."
+			if [[ "$COMP"=="GUTTA_n08" ]] && [[ $ERRORS_UPDATE -eq 0 ]] ; then 
+				echo " -- Errors are about imagick.so library so we can ignore!"
+				echo $LASTRUN > ${LOG_PATH}/last_job_notified.log
+			else
+				echo " -- Errors are real! MUST BE NOTIFIED"
+				echo $LASTRUN > ${LOG_PATH}/last_job_notified.log
+            	Notify 2 "Check" "${COMP} has ${ERRORS} error lines [run: $LASTRUN]"
+
+			fi 
+
+               
+		fi
     fi
 
 done
